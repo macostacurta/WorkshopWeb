539ebb043f1cc892c117428fda161d9a889ed16a
macostacurta <101334491+macostacurta@users.noreply.github.com>
2024-05-07 18:52:00 +0000

    aula 1

---
+++ db/migrations/1715107663563-Create_Users.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+const TABLE_NAME = "users"
+
+export class CreateUsers1715107663563 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner):
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
